d1c1fe21f8676926ef44e6a2f0bcd2a77c64080f
Eric Tendian <erictendian@gmail.com>
2015-07-05 17:20:04 -0500

    New domain for contact form backend

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://dev.iit.io/tendian.io-contact_me.php",
+                url: "http://dev.tendian.io/tendian.io-contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
